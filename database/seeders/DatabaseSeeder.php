56108541ce755ef23fad5905ddc247f506dcae4d
u-m-i <asiessoyeltier1@gmail.com>
2026-04-21 19:59:08 -0500

    fix: seeder bad function call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,27 +9,28 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        if(!env('ADMIN_EMAIL')){
-          // Not seeding
-          return;
-        }
-
-        User::updateOrCreate([
-          'name' => env('ADMIN_NAME'),
-          'email' => env('ADMIN_EMAIL'),
-          'password' => Hash::makeenv('ADMIN_PASSWORD'),
-        ]);
+    if (!env('ADMIN_EMAIL')) {
+      // Not seeding
+      return;
     }
+
+    try {
+      User::factory()->create([
+        'name' => env('ADMIN_NAME'),
+        'email' => env('ADMIN_EMAIL'),
+        'password' => Hash::make(env('ADMIN_PASSWORD')),
+      ]);
+    } catch (\Exception $error) {
+      // Not seeding
+    }
+
+    // User::updateOrCreate([]);
+  }
 }
